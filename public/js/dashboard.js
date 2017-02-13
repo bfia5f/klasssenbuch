739def89ef58eaf42c9900fba50f9ef5e7840f3e
Tobias Stosius <stosius.tobias@gmail.com>
2017-02-13 21:29:25 +0100

    [TASK] Stuff

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,8 +14,11 @@ var cUser = null;
  * CONSTANTS
  */
 const refUsers = "user";
+const refClass = "class";
 const refStudent = refUsers + "/student/";
 const refTeacher = refUsers + "/teacher/";
+// const refTimetable = refClass + "/timetable/";
+const refTimetable = "Timetable/teachers/";
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -30,19 +33,78 @@ firebase.auth().onAuthStateChanged(function(user) {
  * Read once from DB
  */
 function getStudentPromise() {
-  var ref = database.ref(refStudent+"/"+cUser.uid);
-  // TODO: Error handling
+    var ref = database.ref(refStudent + "/" + cUser.uid);
+    // TODO: Error handling
     return ref.once("value").then(function(data) {
         return data.val();
     });
 }
 
+function getAllStudentsPromise() {
+    var ref = database.ref(refStudent);
+    // TODO: Error handling
+    return ref.once("value").then(function(data) {
+        return data.val();
+    });
+}
+
+function getTimetablePromise() {
+    var ref = database.ref(refTimetable);
+    return ref.once("value").then(function(data) {
+        return data.val();
+    });
+}
+
+function getClasslistPromise() {
+    var ref = database.ref(refClass);
+    return ref.once("value").then(function(data) {
+        return data.val();
+    });
+}
+
+function filterStudentsByClass(className) {
+    return getAllStudentsPromise().then(function(data) {
+        var studentsInClass = [];
+        $.each(data, function(studentUID, studentSettings) {
+            if (studentSettings.class === className) {
+                studentsInClass.push(data);
+            }
+        });
+        console.log(studentsInClass);
+        return studentsInClass;
+    });
+}
+
+function createClassDropdown(name, id, classList) {
+    var dropdown_classList = $("<select></select>").attr("id", id).attr("classList_" + "name", name);
+    $.each(classList, function(i, el) {
+        dropdown_classList.append("<option>" + el + "</option>");
+    });
+    $("#setStudentClass").append(dropdown_classList);
+}
+
 
 function updateHTML(userEmail, userName, userProfileImageURL) {
-  // TODO: Error handling
-    getStudentPromise().then(function(student){
-      console.log(student.isFirst);
+    getClasslistPromise().then(function(data) {
+        var keyList = [];
+        $.each(data, function(key, val) {
+            keyList.push(key);
+        });
+        console.log(keyList);
+        createClassDropdown("classdropdown", "classdropdown", keyList);
+        $.each(keyList, function(key, val) {
+            createClassDropdown("studentdropdown", "studentdropdown", filterStudentsByClass(val));
+        });
     });
+
+    // // TODO: Error handling
+    // getTimetablePromise().then(function(timetable) {
+    //     var ele = $.parseHTML(timetable.teacher1);
+    //     console.log($($(timetable.teacher1)));
+    //     $('#stundenplan-placeholder').append($($(timetable.teacher1)));
+    // });
+
+
     $('#user-image')[0].src = userProfileImageURL;
     $('.placeholder').each(function() {
         switch ($(this)[0].innerText) {
@@ -58,14 +120,12 @@ function updateHTML(userEmail, userName, userProfileImageURL) {
 }
 
 function updateProfileInfo(key, value) {
-    console.log(key + " : " + value);
-    console.log(cUser);
     cUser.updateProfile({
         photoURL: value
     }).then(function() {
-        console.log("Updated Profile Info");
+        // console.log("Updated Profile Info");
     }, function(error) {
-        console.log("Something went wrong");
+        // console.log("Something went wrong");
     });
 }
 
@@ -82,8 +142,6 @@ $(document).ready(function() {
     // Profile changes
     $('#save-profile-changes').on('click', function(eventInfo) {
         $('.profile-input').each(function(index) {
-            console.log($(this)[0].id);
-            console.log($(this)[0].value);
             switch ($(this)[0].id) {
                 case 'profile-picture-url':
                     updateProfileInfo('photoURL', $(this)[0].value);
@@ -96,7 +154,6 @@ $(document).ready(function() {
 
     // Logout
     $('#btn-logout').on('click', function() {
-        console.log("click");
         firebase.auth().signOut().then(function() {
             window.location.href = "/logout-success.html"
         }, function(error) {
@@ -108,10 +165,17 @@ $(document).ready(function() {
     /**
      * DEBUG OPTIONS
      */
-
     $('#debug_sendToDb').on('click', function() {
-        database.ref(refStudent + "/" + cUser.uid).set({
-            isFirst: "true"
-        });
+        for (var i = 0; i < 10; i++) {
+            database.ref(refStudent + "/" + i).set({
+                class: "11FI5F",
+                company: "provadis",
+                dateofbirth: "20.07.1993",
+                instructor: "Schmidt",
+                telephone: 12345678,
+                type: "azubi"
+
+            });
+        }
     });
 })
